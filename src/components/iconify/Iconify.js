540876fdccdbc73f7fa262a5d9fbc6699b07d163
Josh McKenney <jmckenney@Josh-McKenneys-MacBook-Pro.local>
2023-04-18 08:39:18 -0700

    fix lint, deploy again

--- src/components/iconify/Iconify.js
+++ src/components/iconify/Iconify.js
@@ -1,14 +1,21 @@
-import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
+import PropTypes from "prop-types";
+import { forwardRef } from "react";
 // icons
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 // @mui
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
 // ----------------------------------------------------------------------
 
+// eslint-disable-next-line react/display-name
 const Iconify = forwardRef(({ icon, width = 20, sx, ...other }, ref) => (
-  <Box ref={ref} component={Icon} icon={icon} sx={{ width, height: width, ...sx }} {...other} />
+  <Box
+    ref={ref}
+    component={Icon}
+    icon={icon}
+    sx={{ width, height: width, ...sx }}
+    {...other}
+  />
 ));
 
 Iconify.propTypes = {
